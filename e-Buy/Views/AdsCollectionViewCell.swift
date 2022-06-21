18c03a7532c712cd416e69b25c05446f13712d58
Rofaida Asem <rofaida.asem@outlook.sa>
2022-06-21 14:34:20 +0200

    first commit

---
+++ e-Buy/Views/AdsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AdsCollectionViewCell.swift
+//  e-Buy
+//
+//  Created by Om Malikah  on 22/11/1443 AH.
+//
+
+import UIKit
+
+class AdsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var adsImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ e-Buy/Views/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  e-Buy
+//
+//  Created by Om Malikah  on 22/11/1443 AH.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var cartButton: UIBarButtonItem!
+    @IBOutlet weak var favoriteButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}

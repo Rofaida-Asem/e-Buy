53d3901f7e67cb610a4dcc69c791a7988ac44495
Rofaida-Asem <107304774+Rofaida-Asem@users.noreply.github.com>
2022-06-22 13:08:01 +0200

    added constent file

---
+++ e-Buy/Constants/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  e-Buy
+//
+//  Created by Amr Abdelnasser on 22/06/2022.
+//
+
+import Foundation
+let productsURL = "https://fde429753a207f610321a557c2e0ceb0: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@menofia-2022-q3.myshopify.com/admin/api/2022-04/products.json"
+
+let customers = "https://fde429753a207f610321a557c2e0ceb0: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@menofia-2022-q3.myshopify.com/admin/api/2022-04/customers.json"
+
+let customCollections = "https://fde429753a207f610321a557c2e0ceb0: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@menofia-2022-q3.myshopify.com/admin/api/2022-04/custom_collections.json"
+
+let orders = "https://fde429753a207f610321a557c2e0ceb0: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@menofia-2022-q3.myshopify.com/admin/api/2022-04/orders.json"
+
+let countries = "https://fde429753a207f610321a557c2e0ceb0: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@menofia-2022-q3.myshopify.com/admin/api/2022-04/countries.json"
+
+let currencies = "https://fde429753a207f610321a557c2e0ceb0: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@menofia-2022-q3.myshopify.com/admin/api/2022-04/currencies.json"
+
+let productsForCustomCollection = "https://fde429753a207f610321a557c2e0ceb0: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@menofia-2022-q3.myshopify.com/admin/api/2022-04/products.json?collection_id=286860542123"
+
+let singleProductUrl = "https://fde429753a207f610321a557c2e0ceb0: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@menofia-2022-q3.myshopify.com/admin/api/2022-04/products/7358110630059.json"

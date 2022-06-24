5f2478f21da02be720986f76bd151017cf7dbb45
amrabobasha <amrabdelnasser2015@gmail.com>
2022-06-24 16:19:24 +0200

    Currencies

---
+++ e-Buy/Model/Currencies/CurrenciesContract.swift
@@ -0,0 +1,8 @@
+//
+//  CurrenciesContract.swift
+//  e-Buy
+//
+//  Created by Amr Abdelnasser on 24/06/2022.
+//
+
+import Foundation
---
+++ e-Buy/Model/Currencies/CurrenctiesModel.swift
@@ -0,0 +1,8 @@
+//
+//  CurrenctiesModel.swift
+//  e-Buy
+//
+//  Created by Amr Abdelnasser on 24/06/2022.
+//
+
+import Foundation

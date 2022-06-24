862558e1297183aa1cd1a55ef2c279a92cee24ca
amrabobasha <amrabdelnasser2015@gmail.com>
2022-06-24 10:00:10 +0200

    modifications on settings

--- e-Buy/Views/Me/SettingsViewController/SettingsViewController.swift
+++ e-Buy/Views/Me/SettingsViewController/SettingsViewController.swift
@@ -18,7 +18,9 @@ class SettingsViewController: UIViewController {
     }
 
     @IBAction func addressButton(_ sender: UIButton) {
-        
+        let addressViewController = self.storyboard?.instantiateViewController(withIdentifier: "AddressViewController") as! AddressViewController
+        addressViewController.modalPresentationStyle = .fullScreen
+        self.present(addressViewController, animated: true)
     }
     
     @IBAction func currencyButton(_ sender: UIButton) {
@@ -32,11 +34,15 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func contactUsButton(_ sender: UIButton) {
-        
+        let contactUsViewController = self.storyboard?.instantiateViewController(withIdentifier: "ContactUsViewController") as! ContactUsViewController
+        contactUsViewController.modalPresentationStyle = .fullScreen
+        self.present(contactUsViewController, animated: true)
     }
     
     @IBAction func aboutUsButton(_ sender: UIButton) {
-        
+        let aboutUsViewController = self.storyboard?.instantiateViewController(withIdentifier: "AboutUsViewController") as! AboutUsViewController
+        aboutUsViewController.modalPresentationStyle = .fullScreen
+        self.present(aboutUsViewController, animated: true)
     }
     
     @IBAction func logOutButton(_ sender: Any) {

74f4fa03097802db35c485c8cab6fe2b3cf3765c
amrabobasha <amrabdelnasser2015@gmail.com>
2022-06-29 12:10:17 +0200

    Commiting Ads and Settings

---
+++ e-Buy/Modules/Settings/AboutUs/AboutUsView/AboutUsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutUsViewController.swift
+//  e-Buy
+//
+//  Created by Amr Abdelnasser on 23/06/2022.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
